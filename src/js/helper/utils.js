c6e0b3570a436fbcff47d0d1d7fcc2028862d00b
jw_0819 <jw_0819@naver.com>
2019-11-25 20:30:09 +0900

    Update BU

--- src/js/helper/utils.js
+++ src/js/helper/utils.js
@@ -7,6 +7,14 @@ class Utils {
   _detectMobileDevice() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
+
+  _detactAgentIE (callBack) {
+    var agent = navigator.userAgent.toLowerCase();
+      if  ((navigator.appName == 'Netscape' && agent.indexOf('trident') != -1) || (agent.indexOf('msie') != -1)) {
+        document.body.classList.add('unsupported-browser');
+        document.getElementById('#wrap').innerHTML = '';
+    }
+  }
 }
 
 export default Utils;
